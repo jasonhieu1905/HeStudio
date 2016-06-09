f70269e0b7d0255304f3d6ba1ae0c0c7dab53ad5
vpnocsen <vpnocsen@gmail.com>
2016-06-09 23:09:04 +0700

    [Begining]  -add login directive  -home directive  -login service

---
+++ HeStudio/src/main/webapp/resources/application/components/home/home.js
@@ -0,0 +1,14 @@
+app.directive('home', function () {
+    return {
+        restrict: 'AE',
+        replace: true,
+        templateUrl: 'resources/application/components/home/home.jsp',
+        scope: {},
+        controller: function ($scope, $element) {
+
+        },
+        link: function () {
+
+        }
+    }
+});
---
+++ HeStudio/src/main/webapp/resources/application/components/home/home.jsp
@@ -0,0 +1,5 @@
+<div>
+    <div class="page-header">
+    <h1>Welcome<small>login successfully</small></h1>
+    </div>
+</div>
